a6c5b5e84d9ec5315e3d8414c20eeab8795b21b6
AustinKerr42 <akerr@iastate.edu>
2017-10-01 22:43:45 -0500

    Need to finish subArray

---
+++ Java/DataStructures/subArray.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    private int arr[];
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        arr[] = new arr[n];
+        
+        for(int a_i=0; a_i < n; a_i++){
+            arr[a_i] = scan.nextInt();
+        }
+        
+        int negativeSums = 0;
+        countNegatives(0, a[n-1]);
+    }
+    
+    public void countNegatives(int first, int last){
+        for(int i=0; i < n; i++{
+            for(l)
+        }
+    }
+}
